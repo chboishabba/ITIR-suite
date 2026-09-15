38f17ce159e3488349b4a5e7a46b5130dfc8340e
Johl Brown <johlbrown@gmail.com>
2026-09-15 16:55:48 +1000

    feat(workbench): add Mabo reading workbench route

---
+++ itir-svelte/src/routes/reading/mabo/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ReadingSurface from '$lib/workbench/ReadingSurface.svelte';
+  import { createMaboReadingSpecimen } from '$lib/workbench/readingSurface.js';
+
+  const specimen = createMaboReadingSpecimen();
+</script>
+
+<ReadingSurface {specimen} />
